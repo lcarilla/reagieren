a34be5196bb7b59c31bdef45fde8013b2804d74f
luis.carilla <luis.carilla@check24.de>
2024-11-01 20:29:27 +0100

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import {useState, useCallback, useRef, createContext, useContext, useEffect, useMemo} from 'react'
+
+export const nutzeStatus = useState
+export const nutzeRueckruf = useCallback
+export const nutzeRef = useRef
+export const erstelleKontext = createContext
+export const nutzeContext = useContext
+export const nutzeEffekt = useEffect
+export const nutzeErinnerung = useMemo
